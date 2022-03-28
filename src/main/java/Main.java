97b79993156b43cbaaed023cffdc584861c2ebf7
AntonovIlya <ilyha.199@mail.ru>
2022-03-28 20:32:35 +0300

    Task2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,21 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -18,7 +28,10 @@ public class Main {
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
-        writeString(json);
+        writeString(json,"data.json");
+        List<Employee> list2 = parseXML("data.xml");
+        String json2 = listToJson(list2);
+        writeString(json2,"data2.json");
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -45,8 +58,8 @@ public class Main {
         return gson.toJson(list, listType);
     }
 
-    private static void writeString(String json) {
-        try (FileWriter file = new FileWriter("data.json")) {
+    private static void writeString(String json, String filename) {
+        try (FileWriter file = new FileWriter(filename)) {
             file.write(json);
             file.flush();
         } catch (IOException e) {
@@ -54,4 +67,40 @@ public class Main {
         }
     }
 
+    private static List<Employee> parseXML(String s) {
+        List<Employee> list = new ArrayList<>(2);
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(s));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                NodeList nodeList1 = nodeList.item(i).getChildNodes();
+                if (nodeList.item(i).getNodeName().equals("employee")) {
+                    Employee employee = new Employee();
+                    for (int j = 0; j < nodeList1.getLength(); j++) {
+                        Node node = nodeList1.item(j);
+                        if (node.ELEMENT_NODE == node.getNodeType()) {
+                            Element element = (Element) node;
+                            String txt = element.getTextContent();
+                            switch (element.getNodeName()) {
+                                case ("id") -> employee.setId(Long.parseLong(txt));
+                                case ("firstName") -> employee.setFirstName(txt);
+                                case ("lastName") -> employee.setLastName(txt);
+                                case ("country") -> employee.setCountry(txt);
+                                case ("age") -> employee.setAge(Integer.parseInt(txt));
+                            }
+                        }
+
+                    }
+                    list.add(employee);
+                }
+
+            }
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.getMessage();
+        }
+        return list;
+    }
 }
